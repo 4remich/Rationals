2a9c81665e79595a9b1d5f4735082fa741c9e654
roman <remich.ua@gmail.com>
2023-01-04 15:21:43 +0200

    add changes after study video has been watched

--- Rationals.py
+++ Rationals.py
@@ -6,64 +6,67 @@ class Rational:
 
         if not isinstance(a, int) or not isinstance(b, int):
             raise TypeError('You must enter an integer number. Repeat the entry, please')
-        if b == 0:
-            raise ValueError('You cannot divide by 0. Enter another number, please')
+        if not b:
+            raise ZeroDivisionError('You cannot divide by 0. Enter another number, please')
 
         self.a = a
         self.b = b
 
     def __mul__(self, other):
+        if isinstance(other, int):
+            other = Rational(other, 1)
+
+        sign = 1 if self.a * self.b > 0 else -1
+
         a = self.a * other.a
         b = self.b * other.b
-        return Rational(a, b)
+        return Rational(sign * a, b)
 
     def __add__(self, other):
+        if isinstance(other, int):
+            other = Rational(other, 1)
+
+        sign = 1 if self.a * self.b > 0 else -1
+
         a = self.a * other.b + other.a * self.b
         b = self.b * other.b
-        return Rational(a, b)
+        return Rational(sign * a, b)
 
     def __sub__(self, other):
+        if isinstance(other, int):
+            other = Rational(other, 1)
+
+        sign = 1 if self.a * self.b > 0 else -1
+
         a = self.a * other.b - other.a * self.b
         b = self.b * other.b
-        return Rational(a, b)
+        return Rational(sign * a, b)
 
     def __floordiv__(self, other):
+        if isinstance(other, int):
+            other = Rational(other, 1)
+
+        sign = 1 if self.a * self.b > 0 else -1
+
         a = self.a * other.b
         b = other.a * self.b
-        return Rational(a, b)
+        return Rational(sign * a, b)
 
     def __str__(self):
 
+        sign = '' if self.a * self.b > 0 else '-'
+        a, b = abs(self.a), abs(self.b)
         d = math.gcd(self.a, self.b)
-        a = self.a // d
-        b = self.b // d
+        a //= d
+        b //= d
 
         if a == b:
-            return '1'
-
-        if not a % b:
-            return f'{a // b}'
-
-        while a < 0 and b < 0:
-
-            if abs(a) < abs(b):
-                return f'{abs(a)}/{abs(b)}'
-
-            if abs(a) > abs(b):
-                return f'{abs(a) // abs(b)} {abs(a) - abs(a) // abs(b) * abs(b)}/{abs(b)}'
-
-        while a < 0 or b < 0:
-
-            if abs(a) < abs(b):
-                return '-' f'{abs(a)}/{abs(b)}'
-
-            if abs(a) > abs(b):
-                return '-' f'{abs(a) // abs(b)} {abs(a) - abs(a) // abs(b) * abs(b)}/{abs(b)}'
-
-        if abs(a) > abs(b) and a > 0 and b > 0:
-            return f'{abs(a) // abs(b)} {abs(a) - abs(a) // abs(b) * abs(b)}/{abs(b)}'
-
-        return f'{a}/{b}'
+            return f'{sign}1'
+        if b == 1:
+            return f'{sign}{a}'
+        if a > b:
+            return f'{sign}{a // b} {a - a // b * b}/{b}'
+        return f'{sign}{a} / {b}'
 
 x = Rational(1, 2)
 y = Rational(-1, 4)
@@ -75,4 +78,5 @@ print(x * y)
 print(x + y)
 print(x - y)
 print(x // y)
+print(x - 2 - y)
 
